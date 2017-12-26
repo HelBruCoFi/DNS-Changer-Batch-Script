271aea7dcf0d1e7b298c32c9f5f35e71017738ee
HelBruCoFi <34587674+HelBruCoFi@users.noreply.github.com>
2017-12-26 12:44:44 +0000

    Update DNS-Google-DHCP.bat

--- DNS-Google-DHCP.bat
+++ DNS-Google-DHCP.bat
@@ -1,27 +1,30 @@
 @echo off
 
-echo MAMAS
+echo Hi!
+echo I am a Batch Script, made by HelBruCoFi, very useful, I can change the DNS of your network cards!!!
+echo.
+echo I can only work with Ethernet and Wi-Fi cards. These must be named this way, if not you can rename them in Control Panel
+echo or just edit my source code with your real network card names.
+echo DON'T forget to RUN ME AS ADMINISTRATOR, in that case I have no rights to live. If you did forget about that, please
+echo kill me and give me life again with truly rights... I accept the pain, I just wanna be useful to you.
 echo.
 
 :Ask
 echo Would you like to change your DNS to Google's or DHCP?
-echo For DHCP option write "1"
-echo For Google DNS write "2"
+echo For DHCP option type and enter "1"
+echo For Google DNS type and enter "2"
 
 set INPUT=
 set /P INPUT=Choose option: %=%
 If %INPUT%==1 goto DHCP
 If %INPUT%==2 goto google
 echo.
-echo You are now a offical retard that can't write 1 or 2
+echo You are now a offical noob that can't write 1 or 2        :^)
 echo.
 echo.
 goto Ask
 
 :DHCP
-
-rem netsh interface ipv4 show interfaces
-rem netsh interface ipv6 show interfaces
 
 netsh interface ipv4 set dnsservers name="Ethernet" source=dhcp
 netsh interface ipv6 set dnsservers name="Ethernet" source=dhcp
@@ -34,26 +37,20 @@ echo.
 
 goto end
 
-:google
 
-rem netsh interface ipv4 show interfaces
-rem netsh interface ipv6 show interfaces
+:google
 
 netsh interface ipv4 add dnsservers "Ethernet" 8.8.8.8
 netsh interface ipv4 add dnsservers "Ethernet" 8.8.4.4
 
-rem >nul sends the command output to nul, so it doesn't print
 netsh interface ipv6 add dnsservers "Ethernet" 2001:4860:4860::8888 >nul
 netsh interface ipv6 add dnsservers "Ethernet" 2001:4860:4860::8844 >nul
-rem "index=2" maybe to insert it on second box
-
 
 netsh interface ipv4 add dnsservers "Wi-Fi" 8.8.8.8
 netsh interface ipv4 add dnsservers "Wi-Fi" 8.8.4.4
 
 netsh interface ipv6 add dnsservers "Wi-Fi" 2001:4860:4860::8888 >nul
 netsh interface ipv6 add dnsservers "Wi-Fi" 2001:4860:4860::8844 >nul
-rem "index=2" maybe to insert it on second box
 
 echo Your DNS is now Google DNS.
 echo.
